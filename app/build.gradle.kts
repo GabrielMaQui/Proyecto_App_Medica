a5abb44fce2604f462eeae85fa70b5d624b4aca9
Gabriel <gmachicaoq@unsa.edu.pe>
2024-06-21 14:27:14 -0500

    Agregar la pantalla principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation("com.google.android.material:material:1.9.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
